6d56bca69275d0ce49519eea1f1750c86a5a8033
Maxime04 <maxime.danino04@gmail.com>
2026-03-17 12:44:23 +0100

    Création de la page mes documents

---
+++ app-crm/frontend/src/pages/MyDocuments.jsx
@@ -0,0 +1,15 @@
+
+
+const MyDocuments = () => {
+    return (
+        <div className="page-container">
+            <h1 className="page-title">Mes documents</h1>
+            <div className="content-card">
+                <p>Liste de vos documents chargés.</p>
+                {/* Futur contenu de la liste des documents */}
+            </div>
+        </div>
+    );
+};
+
+export default MyDocuments;
